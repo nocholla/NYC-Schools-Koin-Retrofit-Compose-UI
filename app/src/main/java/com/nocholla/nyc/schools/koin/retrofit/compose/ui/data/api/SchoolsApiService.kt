f7dc176a67fd1379e2b5a7a4aaaaf932f6512a09
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 12:03:09 -0700

    Update SchoolsApiService.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/api/SchoolsApiService.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/api/SchoolsApiService.kt
@@ -1,4 +1,19 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.api
 
-class SchoolsApiService {
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.model.SchoolDto
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.model.ScoreDto
+import retrofit2.http.GET
+
+interface SchoolsApiService {
+    @GET("s3k6-pzi2.json")
+    suspend fun getSchools(): List<SchoolDto>
+
+    @GET("f9bf-2cp4.json")
+    suspend fun getScores(): List<ScoreDto>
 }
+
+// Schools JSON
+// https://data.cityofnewyork.us/resource/s3k6-pzi2.json
+
+// Scores JSON
+// https://data.cityofnewyork.us/resource/f9bf-2cp4.json
