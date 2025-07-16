50d5921b59621d847f13c72db60f13647997ba6c
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 12:04:08 -0700

    Update SchoolDao.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/dao/SchoolDao.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/dao/SchoolDao.kt
@@ -1,4 +1,22 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.dao
 
-class SchoolDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.model.SchoolEntity
+
+@Dao
+interface SchoolDao {
+    @Query("SELECT * FROM schools")
+    suspend fun getAllSchools(): List<SchoolEntity>
+
+    @Query("SELECT * FROM schools WHERE dbn = :dbn")
+    suspend fun getSchoolByDbn(dbn: String): SchoolEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllSchools(schools: List<SchoolEntity>)
+
+    @Query("DELETE FROM schools")
+    suspend fun deleteAllSchools()
 }
