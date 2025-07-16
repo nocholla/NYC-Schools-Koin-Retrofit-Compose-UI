5002f6dc54627a5ed1b809b1ae1a1c27f3e2f77c
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 12:08:26 -0700

    Update SchoolRepository.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/domain/repository/SchoolRepository.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/domain/repository/SchoolRepository.kt
@@ -1,4 +1,10 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.domain.repository
 
-class SchoolRepository {
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.domain.model.School
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.domain.model.Score
+import kotlinx.coroutines.flow.Flow
+
+interface SchoolRepository {
+    suspend fun getSchools(): Flow<List<School>>
+    suspend fun getScores(): Flow<List<Score>>
 }
