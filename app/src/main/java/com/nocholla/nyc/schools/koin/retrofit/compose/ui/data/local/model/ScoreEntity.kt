8a4b38bb15137dc168dbd292f06d46deb0bc813c
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 12:01:06 -0700

    Update ScoreEntity.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/model/ScoreEntity.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/model/ScoreEntity.kt
@@ -1,4 +1,24 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.model
 
-class ScoreEntity {
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.domain.model.Score
+
+@Entity(tableName = "scores")
+data class ScoreEntity(
+    @PrimaryKey val dbn: String,
+    val schoolName: String?,
+    val numOfSatTestTakers: String?,
+    val satCriticalReadingAvgScore: String?,
+    val satMathAvgScore: String?,
+    val satWritingAvgScore: String?
+) {
+    fun toDomain() = Score(
+        dbn = dbn,
+        schoolName = schoolName,
+        numOfSatTestTakers = numOfSatTestTakers,
+        satCriticalReadingAvgScore = satCriticalReadingAvgScore,
+        satMathAvgScore = satMathAvgScore,
+        satWritingAvgScore = satWritingAvgScore
+    )
 }
