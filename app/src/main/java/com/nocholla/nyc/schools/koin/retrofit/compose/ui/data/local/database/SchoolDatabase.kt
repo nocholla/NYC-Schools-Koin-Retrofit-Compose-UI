b70caf80fb2c57ca87242faa9d40a55114eeef3a
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 12:05:59 -0700

    Update SchoolDatabase.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/database/SchoolDatabase.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/database/SchoolDatabase.kt
@@ -1,4 +1,14 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.database
 
-class SchoolDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.dao.SchoolDao
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.dao.ScoreDao
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.model.SchoolEntity
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.model.ScoreEntity
+
+@Database(entities = [SchoolEntity::class, ScoreEntity::class], version = 1, exportSchema = false)
+abstract class SchoolDatabase : RoomDatabase() {
+    abstract fun schoolDao(): SchoolDao
+    abstract fun scoreDao(): ScoreDao
 }
