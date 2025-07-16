29c67e3e44f6c79bd145a9efbc2f5d9e4615ad53
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 11:59:02 -0700

    Update ScoreDto.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/model/ScoreDto.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/model/ScoreDto.kt
@@ -1,4 +1,22 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.model
 
-class ScoreDto {
+import com.google.gson.annotations.SerializedName
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.domain.model.Score
+
+data class ScoreDto(
+    @SerializedName("dbn") val dbn: String?,
+    @SerializedName("school_name") val schoolName: String?,
+    @SerializedName("num_of_sat_test_takers") val numOfSatTestTakers: String?,
+    @SerializedName("sat_critical_reading_avg_score") val satCriticalReadingAvgScore: String?,
+    @SerializedName("sat_math_avg_score") val satMathAvgScore: String?,
+    @SerializedName("sat_writing_avg_score") val satWritingAvgScore: String?
+) {
+    fun toDomain() = Score(
+        dbn = dbn ?: "",
+        schoolName = schoolName ?: "",
+        numOfSatTestTakers = numOfSatTestTakers ?: "",
+        satCriticalReadingAvgScore = satCriticalReadingAvgScore ?: "",
+        satMathAvgScore = satMathAvgScore ?: "",
+        satWritingAvgScore = satWritingAvgScore ?: ""
+    )
 }
