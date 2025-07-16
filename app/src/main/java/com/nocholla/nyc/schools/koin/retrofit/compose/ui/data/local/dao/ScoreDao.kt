a1b039b27df941a7f91b118975d08169479a7e29
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 12:04:53 -0700

    Update ScoreDao.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/dao/ScoreDao.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/local/dao/ScoreDao.kt
@@ -1,4 +1,22 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.dao
 
-class ScoreDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.local.model.ScoreEntity
+
+@Dao
+interface ScoreDao {
+    @Query("SELECT * FROM scores")
+    suspend fun getAllScores(): List<ScoreEntity>
+
+    @Query("SELECT * FROM scores WHERE dbn = :dbn")
+    suspend fun getScoreByDbn(dbn: String): ScoreEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAllScores(scores: List<ScoreEntity>)
+
+    @Query("DELETE FROM scores")
+    suspend fun deleteAllScores()
 }
