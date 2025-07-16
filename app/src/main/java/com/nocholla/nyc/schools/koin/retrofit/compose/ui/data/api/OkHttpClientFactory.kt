df88a1e232493a3ef290bc8c09a3be091b93dcca
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 11:54:34 -0700

    Update OkHttpClientFactory.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/api/OkHttpClientFactory.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/data/api/OkHttpClientFactory.kt
@@ -1,4 +1,19 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.data.api
 
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import java.util.concurrent.TimeUnit
+
 class OkHttpClientFactory {
+    fun createOkHttpClient(): OkHttpClient {
+        val loggingInterceptor = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+        return OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
+    }
 }
