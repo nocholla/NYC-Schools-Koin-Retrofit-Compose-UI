b66ba5b870c025791260146826222378d0354e70
NICHOLAS OCHOLLA <nocholla@yahoo.com>
2025-07-16 11:55:36 -0700

    Update Score.kt

--- app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/domain/model/Score.kt
+++ app/src/main/java/com/nocholla/nyc/schools/koin/retrofit/compose/ui/domain/model/Score.kt
@@ -1,4 +1,24 @@
 package com.nocholla.nyc.schools.koin.retrofit.compose.ui.domain.model
 
-class Score {
-}
+data class Score(
+    val dbn: String?,
+    val schoolName: String?,
+    val numOfSatTestTakers: String?,
+    val satCriticalReadingAvgScore: String?,
+    val satMathAvgScore: String?,
+    val satWritingAvgScore: String?
+)
+
+/*
+https://data.cityofnewyork.us/resource/f9bf-2cp4.json
+[
+   {
+      "dbn":"01M292",
+      "school_name":"HENRY STREET SCHOOL FOR INTERNATIONAL STUDIES",
+      "num_of_sat_test_takers":"29",
+      "sat_critical_reading_avg_score":"355",
+      "sat_math_avg_score":"404",
+      "sat_writing_avg_score":"363"
+   }
+]
+*/
